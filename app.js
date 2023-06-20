ab4c8a8fc4d7d9e6925e8519275220c94bab170c
Angel Alegre <aalegre.qa@gmail.com>
2023-06-20 14:03:13 -0500

    enviando el mensaje a un archivo

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === "/") {
+    //console.log(req.url, req.method, req.headers); -- para obtener info
+    //process.exit();    -- con esto sales
+
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
     res.write(
@@ -12,8 +17,14 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
-  //console.log(req.url, req.method, req.headers);
-  //process.exit();
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first page web</title></head>");
